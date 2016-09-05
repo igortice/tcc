29b3c1fea53a5b5422e99d2144ef659c4aa5d0d6
Igor Rocha <igortice@gmail.com>
2016-09-05 15:46:35 -0300

    add flash message notice

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 20160905165229) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
-    t.string   "text"
+    t.text     "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
